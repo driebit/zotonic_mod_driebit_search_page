426403c1aa5080f96cd80a314e1c8b707995a2ff
Emma van Proosdij <emma@driebit.nl>
2025-07-11 15:45:26 +0200

    Basic repository setup

---
+++ src/mod_driebit_calendar.erl
@@ -0,0 +1,14 @@
+%% @author Driebit <tech@driebit.nl>
+%% @copyright 2025
+
+-module(mod_driebit_search_page).
+-author("Driebit <tech@driebit.nl>").
+
+-mod_title("Driebit search page").
+-mod_description("Configurable search page").
+
+-include_lib("zotonic_core/include/zotonic.hrl").
+
+-mod_prio(500).
+
+-export([]).
