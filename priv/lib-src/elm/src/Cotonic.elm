d554fe4305a30c15b57c50d0a6559fa45fd2f1f2
Emma <emma@driebit.nl>
2025-11-04 10:11:35 +0100

    Fix duplicate keys

--- priv/lib-src/elm/src/Cotonic.elm
+++ priv/lib-src/elm/src/Cotonic.elm
@@ -7,7 +7,12 @@ type alias CotonicCall =
     { topic : String
     , parameters : List ( String, Encode.Value )
     , replyTopic : String
+    , encodeMethod : EncodeMethod
     }
+
+type EncodeMethod =
+    Object
+    | List
 
 
 searchPageTopic : List ( String, Encode.Value ) -> CotonicCall
@@ -15,6 +20,7 @@ searchPageTopic parameters =
     { topic = "bridge/origin/model/search/get"
     , parameters = parameters
     , replyTopic = "SearchReply"
+    , encodeMethod = List
     }
 
 
@@ -23,13 +29,29 @@ templateTopic id =
     { topic = "bridge/origin/model/template/get/render/search_result.tpl"
     , parameters = [ ( "id", Encode.int id ) ]
     , replyTopic = "TemplateReply/" ++ String.fromInt id
+    , encodeMethod = Object
     }
-
 
 toJson : CotonicCall -> Encode.Value
 toJson call =
-    Encode.object
-        [ ( "topic", Encode.string call.topic )
-        , ( "parameters", Encode.object call.parameters )
-        , ( "replyTopic", Encode.string call.replyTopic )
-        ]
+    case call.encodeMethod of 
+        List -> 
+            Encode.object
+                [ ( "topic", Encode.string call.topic )
+                , ( "parameters"
+                , Encode.list
+                        (\( key, value ) ->
+                            Encode.object
+                                [ ( key, value )
+                                ]
+                        )
+                        call.parameters
+                )
+                , ( "replyTopic", Encode.string call.replyTopic )
+                ]
+        Object -> 
+            Encode.object
+                [ ( "topic", Encode.string call.topic )
+                , ( "parameters", Encode.object call.parameters )
+                , ( "replyTopic", Encode.string call.replyTopic )
+                ]
